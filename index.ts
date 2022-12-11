28bf50d12d72a80b882468e1ffdda4ca2a57cdbe
ADaunys <augustas.daunys@ktu.edu>
2022-12-11 16:29:29 +0200

    Sequelize complete - TESTED

--- index.ts
+++ index.ts
@@ -29,174 +29,198 @@ import express from "express";
 import { DataTypes, Sequelize } from "sequelize";
 //import * as mysql2 from "mysql2";
 
+const port = process.env.PORT || 3000;
+const app = express();
+app.use(express.json());
+
+let tempStiprumasArray: any[] = [];
+let tempVartotojaiArray: any[] = [];
+let tempMatavimaiArray: any[] = [];
+const users: User[] = [];
+let matavimai: Matavimas[] = [];
+let grid: number[][] = [];
+
+app.listen(port, async () => {
+  console.log(`Timezones by location application is running on port ${port}.`);
+  tempStiprumasArray = await tempStiprumas.findAll();
+  tempMatavimaiArray = await tempMatavimai.findAll();
+  tempVartotojaiArray = await tempVartotojai.findAll();
+
+  const stiprumai = tempStiprumasArray as Stiprumas[];
+  const vartotojai = tempVartotojaiArray as Vartotojas[];
+
+  let macAddresses: string[] = vartotojai.map(
+    (vartotojas: Vartotojas) => vartotojas.mac
+  );
+  macAddresses = macAddresses.filter(
+    (mac: string, index: number) => macAddresses.indexOf(mac) === index
+  );
+
+  macAddresses.forEach((mac: string) => {
+    const user: User = {
+      mac: mac,
+      stiprumai: [],
+    };
+    vartotojai
+      .filter((vartotojas: Vartotojas) => vartotojas.mac === mac)
+      .forEach((vartotojas: Vartotojas) => {
+        const stiprumas: Stiprumas = {
+          sensorius: vartotojas.sensorius,
+          stiprumas: vartotojas.stiprumas,
+        };
+        user.stiprumai.push(stiprumas);
+      });
+
+    users.push(user);
+  });
+
+  matavimai = tempMatavimaiArray.map((m: any): Matavimas => {
+    return {
+      matavimas: m.matavimas,
+      x: m.x,
+      y: m.y,
+      atstumas: m.atstumas,
+      stiprumai: stiprumai.filter(
+        (s: Stiprumas) => s.matavimas === m.matavimas
+      ),
+    };
+  });
+
+  let maxY = matavimai.reduce(
+    (max: number, p: { y: number }) => (p.y > max ? p.y : max),
+    matavimai[0].y
+  );
+  let maxX = matavimai.reduce(
+    (max: number, p: { x: number }) => (p.x > max ? p.x : max),
+    matavimai[0].x
+  );
+  let minY = matavimai.reduce(
+    (min: number, p: { y: number }) => (p.y < min ? p.y : min),
+    matavimai[0].y
+  );
+  let minX = matavimai.reduce(
+    (min: number, p: { x: number }) => (p.x < min ? p.x : min),
+    matavimai[0].x
+  );
+  // correct min and max values to start from 0
+  maxY -= minY;
+  maxX -= minX;
+  matavimai.forEach((p: { x: number; y: number }) => {
+    p.x -= minX;
+    p.y -= minY;
+  });
+
+  // create a 2D array of zeros
+  for (let y = 0; y <= maxY; y++) {
+    grid[y] = [];
+    for (let x = 0; x <= maxX; x++) {
+      if (
+        matavimai.find((p: { x: number; y: number }) => p.x === x && p.y === y)
+      ) {
+        grid[y][x] = 1;
+      } else {
+        grid[y][x] = 0;
+      }
+    }
+  }
+
+  grid.forEach((row: number[]) => {
+    console.log(row.join(""));
+  });
+});
+
 export const sequelize = new Sequelize("LDB", "stud", "vLXCDmSG6EpEnhXX", {
-  host: "http://seklys.ila.lt:5080",
+  host: "seklys.ila.lt",
+  port: 3306,
   dialect: "mysql",
 });
 
-const tempStiprumas = sequelize.define("stiprumai", {
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
+const tempStiprumas = sequelize.define(
+  "stiprumai",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+    },
+    matavimas: DataTypes.INTEGER,
+    sensorius: DataTypes.STRING,
+    stiprumas: DataTypes.FLOAT,
   },
-  matavimas: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-  },
-  sensorius: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  stiprumas: {
-    type: DataTypes.FLOAT,
-    allowNull: false,
-  },
-});
+  {
+    timestamps: false,
+    freezeTableName: true,
+    tableName: "stiprumai",
+  }
+);
 
-const tempMatavimai = sequelize.define("matavimai", {
-  matavimas: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
+const tempMatavimai = sequelize.define(
+  "matavimai",
+  {
+    matavimas: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    x: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    y: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    atstumas: {
+      type: DataTypes.FLOAT,
+      allowNull: false,
+    },
   },
-  x: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-  },
-  y: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-  },
-  atstumas: {
-    type: DataTypes.FLOAT,
-    allowNull: false,
-  },
-});
+  {
+    timestamps: false,
+    freezeTableName: true,
+    tableName: "matavimai",
+  }
+);
 
-const tempVartotojai = sequelize.define("vartotojai", {
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
+const tempVartotojai = sequelize.define(
+  "vartotojai",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    mac: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    sensorius: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    stiprumas: {
+      type: DataTypes.FLOAT,
+      allowNull: false,
+    },
   },
-  mac: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  sensorius: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  stiprumas: {
-    type: DataTypes.FLOAT,
-    allowNull: false,
-  },
-});
-
-const tempStiprumasArray = await tempStiprumas.findAll();
-const tempMatavimaiArray = await tempMatavimai.findAll();
-const tempVartotojaiArray = await tempVartotojai.findAll();
+  {
+    timestamps: false,
+    freezeTableName: true,
+    tableName: "vartotojai",
+  }
+);
 
 //const tempMatavimai = require("./data/matavimai.json");
 //const tempStiprumas = require("./data/stiprumai.json");
 //const tempVartotojai = require("./data/vartotojai.json");
-const stiprumai = tempStiprumasArray as Stiprumas[];
-const vartotojai = tempVartotojaiArray as Vartotojas[];
-
-const app = express();
-app.use(express.json());
 
 app.use((req, res, next) => {
   console.log(`${req.method} ${req.path} - ${req.ip}`);
   next();
 });
 
-const port = process.env.PORT || 3000;
 const WILI_BOX_1 = "wiliboxas1";
 const WILI_BOX_2 = "wiliboxas2";
 const WILI_BOX_3 = "wiliboxas3";
-
-const users: User[] = [];
-let macAddresses: string[] = vartotojai.map(
-  (vartotojas: Vartotojas) => vartotojas.mac
-);
-macAddresses = macAddresses.filter(
-  (mac: string, index: number) => macAddresses.indexOf(mac) === index
-);
-
-macAddresses.forEach((mac: string) => {
-  const user: User = {
-    mac: mac,
-    stiprumai: [],
-  };
-  vartotojai
-    .filter((vartotojas: Vartotojas) => vartotojas.mac === mac)
-    .forEach((vartotojas: Vartotojas) => {
-      const stiprumas: Stiprumas = {
-        sensorius: vartotojas.sensorius,
-        stiprumas: vartotojas.stiprumas,
-      };
-      user.stiprumai.push(stiprumas);
-    });
-
-  users.push(user);
-});
-
-// console.table(users.map((user: User) => user.stiprumai));
-
-const matavimai: Matavimas[] = tempMatavimaiArray.map((m: any): Matavimas => {
-  return {
-    matavimas: m.matavimas,
-    x: m.x,
-    y: m.y,
-    atstumas: m.atstumas,
-    stiprumai: stiprumai.filter((s: Stiprumas) => s.matavimas === m.matavimas),
-  };
-});
-
-let maxY = matavimai.reduce(
-  (max: number, p: { y: number }) => (p.y > max ? p.y : max),
-  matavimai[0].y
-);
-let maxX = matavimai.reduce(
-  (max: number, p: { x: number }) => (p.x > max ? p.x : max),
-  matavimai[0].x
-);
-let minY = matavimai.reduce(
-  (min: number, p: { y: number }) => (p.y < min ? p.y : min),
-  matavimai[0].y
-);
-let minX = matavimai.reduce(
-  (min: number, p: { x: number }) => (p.x < min ? p.x : min),
-  matavimai[0].x
-);
-// correct min and max values to start from 0
-maxY -= minY;
-maxX -= minX;
-matavimai.forEach((p: { x: number; y: number }) => {
-  p.x -= minX;
-  p.y -= minY;
-});
-
-// create a 2D array of zeros
-let grid: number[][] = [];
-for (let y = 0; y <= maxY; y++) {
-  grid[y] = [];
-  for (let x = 0; x <= maxX; x++) {
-    if (
-      matavimai.find((p: { x: number; y: number }) => p.x === x && p.y === y)
-    ) {
-      grid[y][x] = 1;
-    } else {
-      grid[y][x] = 0;
-    }
-  }
-}
-
-grid.forEach((row: number[]) => {
-  console.log(row.join(""));
-});
 
 app.get("/", (req, res) => {
   let string = "";
@@ -304,8 +328,4 @@ app.delete("/signals/:id", (req, res) => {
   } else {
     res.status(404).end();
   }
-});
-
-app.listen(port, () => {
-  console.log(`Timezones by location application is running on port ${port}.`);
 });
